c43593cf28d8d58b06987cdaf437b7e64c8dd029
Cerdic <cedric@yterium.com>
2014-01-11 14:33:29 +0000

    Report de r79138 : Affichage par défaut des raccourcis ressource <http://...> Attention : propre génère par défaut un <p> autour de son contenu même si celui-ci ne contient qu'une ligne. Du coup les <http://...> se retrouvaient tout seul dans un <p></p> qui cassait le paragraphe dans lequel ils étaient insérés.
    On corrige pour avoir un affichage par défaut le plus neutre possible

--- inc/ressource.php
+++ inc/ressource.php
@@ -18,17 +18,17 @@ function traiter_ressources($r) {
 		$url = $url[0];
 		# <http://url/absolue>
 		if (preg_match(',^https?://,i', $url))
-			$html = propre("<span class='ressource spip_out'>&lt;[->".$url."]&gt;</span>");
+			$html = PtoBR(propre("<span class='ressource spip_out'>&lt;[->".$url."]&gt;</span>"));
 		# <url/relative>
 		else if (false !== strpos($url, '/'))
-			$html = propre("<span class='ressource spip_in'>&lt;[->".$url."]&gt;</span>");
+			$html = PtoBR(propre("<span class='ressource spip_in'>&lt;[->".$url."]&gt;</span>"));
 		# <fichier.rtf>
 		else {
 			preg_match(',\.([^.]+)$,', $url, $regs);
 			if (file_exists($f = _DIR_IMG.$regs[1].'/'.$url)) {
-				$html = propre("<span class='ressource spip_in'>&lt;[".$url."->".$f."]&gt;</span>");
+				$html = PtoBR(propre("<span class='ressource spip_in'>&lt;[".$url."->".$f."]&gt;</span>"));
 			} else {
-				$html = propre("<span class='ressource'>&lt;".$url."&gt;</span>");
+				$html = PtoBR(propre("<span class='ressource'>&lt;".$url."&gt;</span>"));
 			}
 		}
 	}

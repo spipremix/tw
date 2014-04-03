26cd528af25372189fb9d8d59a360a40944ecf03
Fil <fil@rezo.net>
2014-04-03 13:08:40 +0000

    figure et figcaption sont des blocks

--- wheels/spip/spip-paragrapher.php
+++ wheels/spip/spip-paragrapher.php
@@ -17,7 +17,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @return string
  */
 function fermer_para_mano(&$t) {
-	# match: ",<p (.*)<(/?)(STOP P|div|pre|ul|ol|li|blockquote|h[1-6r]|t(able|[rdh]|head|body|foot|extarea)|form|object|center|marquee|address|applet|iframe|d[ltd]|script|noscript|map|button|fieldset|style)\b,UimsS"
+	# match: ",<p (.*)<(/?)(STOP P|div|pre|ul|ol|li|blockquote|h[1-6r]|t(able|[rdh]|head|body|foot|extarea)|form|object|center|marquee|address|applet|iframe|figure|figcaption|d[ltd]|script|noscript|map|button|fieldset|style)\b,UimsS"
 	# replace: "\n<p "+trim($1)+"</p>\n<$2$3"
 
 	foreach (explode('<p ', $t) as $c => $p) {
@@ -26,7 +26,7 @@ function fermer_para_mano(&$t) {
 		else {
 			$pi = strtolower($p);
 			if (preg_match(
-			",</?(?:stop p|div|pre|ul|ol|li|blockquote|h[1-6r]|t(able|[rdh]|head|body|foot|extarea)|form|object|center|marquee|address|applet|iframe|d[ltd]|script|noscript|map|button|fieldset|style)\b,S",
+			",</?(?:stop p|div|pre|ul|ol|li|blockquote|h[1-6r]|t(able|[rdh]|head|body|foot|extarea)|form|object|center|marquee|address|applet|iframe|figure|figcaption|d[ltd]|script|noscript|map|button|fieldset|style)\b,S",
 			$pi, $r)) {
 				$pos = strpos($pi, $r[0]);
 				$t .= "<p ".str_replace("\n", _AUTOBR."\n", rtrim(substr($p,0,$pos)))."</p>\n".substr($p,$pos);

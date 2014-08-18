c9b175c1b72a278aca78f6265c5df740a262128a
Fil <fil@rezo.net>
2014-08-18 19:16:35 +0000

    une solution pourrite pour avoir une surcharge dans un plugin d'un fichier défini dans un pipeline : utiliser un fichier qui charge celui qu'on veut par include_spip() ; en effet inc/plugin.php appelle *en dur* le fichier du plugin, ce qui interdit les surchages. Pour moi c'est une incohérence, mais bon c'est comme ça... à revoir peut-être (annule au passage r84263 qui revertait r81717 )

--- inc/ressource.php
+++ inc/ressource.php
@@ -9,30 +9,45 @@
 define('_EXTRAIRE_RESSOURCES', ',' . '<"?(https?://|[^\s][\w -]+\.[\w -]+)[^<]*>'.',UimsS');
 
 
+/* pipeline pour typo */
+function ressource_post_typo($t) {
+	if (strpos($t, '<') !== false) {
+		$t = preg_replace_callback(_EXTRAIRE_RESSOURCES, 'traiter_ressources', $t);
+	}
+	return $t;
+}
+
+/* pipeline pour propre */
+function ressource_pre_liens($t) {
+	if (strpos($t, '<') !== false) {
+		$t = preg_replace_callback(_EXTRAIRE_RESSOURCES, 'traiter_ressources', $t);
+
+		// echapper les autoliens eventuellement inseres (en une seule fois)
+		if (strpos($t,"<html>")!==false)
+			$t = echappe_html($t);
+	}
+	return $t;
+}
+
 function traiter_ressources($r) {
 	$html = null;
-	if ($ressource = charger_fonction('ressource', 'inc', true)) {
-		$html = $ressource($r[0]);
-	}
 
-	if (is_null($html)) {
-		include_spip('inc/lien');
-		$url = explode(' ', trim($r[0], '<>'));
-		$url = $url[0];
-		# <http://url/absolue>
-		if (preg_match(',^https?://,i', $url))
-			$html = PtoBR(propre("<span class='ressource spip_out'>&lt;[->".$url."]&gt;</span>"));
-		# <url/relative>
-		else if (false !== strpos($url, '/'))
-			$html = PtoBR(propre("<span class='ressource spip_in'>&lt;[->".$url."]&gt;</span>"));
-		# <fichier.rtf>
-		else {
-			preg_match(',\.([^.]+)$,', $url, $regs);
-			if (file_exists($f = _DIR_IMG.$regs[1].'/'.$url)) {
-				$html = PtoBR(propre("<span class='ressource spip_in'>&lt;[".$url."->".$f."]&gt;</span>"));
-			} else {
-				$html = PtoBR(propre("<span class='ressource'>&lt;".$url."&gt;</span>"));
-			}
+	include_spip('inc/lien');
+	$url = explode(' ', trim($r[0], '<>'));
+	$url = $url[0];
+	# <http://url/absolue>
+	if (preg_match(',^https?://,i', $url))
+		$html = PtoBR(propre("<span class='ressource spip_out'>&lt;[->".$url."]&gt;</span>"));
+	# <url/relative>
+	else if (false !== strpos($url, '/'))
+		$html = PtoBR(propre("<span class='ressource spip_in'>&lt;[->".$url."]&gt;</span>"));
+	# <fichier.rtf>
+	else {
+		preg_match(',\.([^.]+)$,', $url, $regs);
+		if (file_exists($f = _DIR_IMG.$regs[1].'/'.$url)) {
+			$html = PtoBR(propre("<span class='ressource spip_in'>&lt;[".$url."->".$f."]&gt;</span>"));
+		} else {
+			$html = PtoBR(propre("<span class='ressource'>&lt;".$url."&gt;</span>"));
 		}
 	}
 
